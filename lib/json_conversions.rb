405e962489377b440b73e34154ddb256f3b6cc39
ddavisgraphics <dave@ddavisgraphics.com>
2018-02-04 12:25:26 -0500

    better code documentation and tests.

--- lib/json_conversions.rb
+++ lib/json_conversions.rb
@@ -1,21 +1,46 @@
 require 'json_conversions/version'
 require 'json'
 
+# Namespace for classes and that handle json conversions
+# @since 0.1.0
 module JsonConversions
-  # Validate JSON
+  # Handler class parses the json and preps it for next stages based on given arguments
+  #
+  # @author David J. Davis
+  # @since 0.1.0
+  # @attr_accessor :json the parsed json that was sent into the document 
+  # @attr_accessor :raw_json the string or file given when the handler class was instantied 
   class Handler
-    attr_accessor :json
-    def self.valid(json)
+    attr_accessor :json, :raw_json
+
+    # Sets raw json on instantiation no return value 
+    #
+    # @author David J. Davis
+    def initialize(json)
+      @raw_json = json
+    end
+
+    # The method that validates json and sets up for the next step in parsing the document.
+    # Failure will result in a system error being thrown. 
+    #
+    # @author David J. Davis
+    #
+    # @return [Hash] is the hash of the json contents
+    def valid
       begin
-        if json.end_with? '.json'
-          file = File.read(json)
+        if @raw_json.end_with? '.json'
+          file = File.read(@raw_json)
           @json = JSON.parse(file)
         else
-          @json = JSON.parse(json)
+          @json = JSON.parse(@raw_json)
         end
       rescue JSON::ParserError => e
         raise StandardError, 'Error: wrong filetype or invalid json ' + e.message
       end
+    end
+
+    def conversion(type)
+      puts "Type of conversion #{type}"
     end
   end
 
--- spec/json_conversions_spec.rb
+++ spec/json_conversions_spec.rb
@@ -7,27 +7,68 @@ describe JsonConversions do
 
   it 'validates a valid json string, also returns a hash' do
     test_json = '{"id": 1, "first_name": "Berry", "last_name": "Brealey"}'
-    type = JsonConversions::Handler.valid(test_json).class
-    expect(JsonConversions::Handler.valid(test_json)).to be_truthy
+    handler = JsonConversions::Handler.new(test_json)
+    type = handler.valid.class
+    expect(handler.valid).to be_truthy
     expect(type).to eq(Hash)
   end
 
   it 'tests if json string is invalid and throws an exception' do
     test_json = '{"id": 1 "firstname": "Berry"}'
-    expect { JsonConversions::Handler.valid(test_json) }.to raise_error(/Error: wrong filetype or invalid json/)
+    handler = JsonConversions::Handler.new(test_json) 
+    expect { handler.valid }.to raise_error(/Error: wrong filetype or invalid json/)
   end
 
   it 'takes a valid json file and returns the result if valid' do
     test_json = './spec/files/valid.json'
-    type = JsonConversions::Handler.valid(test_json).class
-    expect(JsonConversions::Handler.valid(test_json)).to be_truthy
+    handler = JsonConversions::Handler.new(test_json)
+    type = handler.valid.class
+    expect(handler.valid).to be_truthy
     expect(type).to eq(Hash)
   end
 
   it 'tests invalid json file and raises an exception' do
     test_json = './spec/files/invalid.json'
-    expect { JsonConversions::Handler.valid(test_json) }.to raise_error(/Error: wrong filetype or invalid json/)
+    handler = JsonConversions::Handler.new(test_json)
+    expect { handler.valid }.to raise_error(/Error: wrong filetype or invalid json/)
   end
+
+  it 'allows for getting the valid json from the handler method' do
+    json = './spec/files/valid.json' 
+    handler = JsonConversions::Handler.new(json)
+    valid_json = handler.valid
+    accessor_json = handler.json
+    expect(valid_json).to eq(accessor_json)
+  end
+
+  it 'allows for getting the raw json value back that was fed upon instantiation' do
+    json = './spec/files/valid.json'
+    handler = JsonConversions::Handler.new(json)
+    expect(json).to eq(handler.raw_json)
+  end
+
+  it 'sets raw json seperatly' do
+    json = './spec/files/valid.json'
+    new_json = './spec/files/large_valid.json'
+    handler = JsonConversions::Handler.new(json)
+    handler.raw_json = new_json
+    expect(handler.raw_json).to eq(new_json)
+  end
+
+  it 'validate raw json after setting it manually' do
+    test_json = './spec/files/valid.json'
+    new_json = './spec/files/large_valid.json'
+    new_test_json = JSON.parse(File.read(new_json))
+    handler = JsonConversions::Handler.new(test_json)
+    handler.raw_json = new_json
+    json = handler.valid
+    expect(json).to be_truthy
+    expect(json).to eq(new_test_json)
+  end
+
+  it '' do
+  
+  end 
 
   it 'turns key value pairs into simple html' do
     key = 'somekey'
@@ -35,5 +76,4 @@ describe JsonConversions do
     return_html = "<div class='#{key}'> <strong> #{key.capitalize} </strong> #{value} </div>"
     expect(JsonConversions::Html.markup(key, value)).to eq(return_html)
   end
-
 end
