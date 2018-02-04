7ca3a48cc0e8454e02c09db0a11444e439a8a4ec
ddavisgraphics <dave@ddavisgraphics.com>
2018-02-03 20:56:45 -0500

    Passing tests and better Handler Method.  Although not sure about using this method instantiat

--- lib/json_conversions.rb
+++ lib/json_conversions.rb
@@ -3,15 +3,30 @@ require 'json'
 
 module JsonConversions
   # Validate JSON
-  class Validate
-    def self.valid?(json)
-      !!JSON.parse(json) rescue false
+  class Handler
+    attr_accessor :json
+    def self.valid(json)
+      begin
+        if json.end_with? '.json'
+          file = File.read(json)
+          @json = JSON.parse(file)
+        else
+          @json = JSON.parse(json)
+        end
+      rescue JSON::ParserError => e
+        raise StandardError, 'Error: wrong filetype or invalid json ' + e.message
+      end
     end
   end
 
-  # class Html 
-  #   def initialize(json)
-  #     @json = json 
-  #   end
-  # end
+  class Html
+    attr_accessor :json
+    def initialize(json)
+      @json = JSON.parse(json)
+    end
+
+    def self.markup(key,value)
+      "<div class='#{key}'> <strong> #{key.capitalize} </strong> #{value} </div>"
+    end
+  end
 end
--- spec/json_conversions_spec.rb
+++ spec/json_conversions_spec.rb
@@ -1,17 +1,39 @@
 require "spec_helper"
 
 describe JsonConversions do
-  it "has a version number" do
+  it 'has a version number' do
     expect(JsonConversions::VERSION).not_to be nil
   end
 
-  it 'validates a valid json string' do
+  it 'validates a valid json string, also returns a hash' do
     test_json = '{"id": 1, "first_name": "Berry", "last_name": "Brealey"}'
-    expect(JsonConversions::Validate.valid?(test_json)).to eq(true)
+    type = JsonConversions::Handler.valid(test_json).class
+    expect(JsonConversions::Handler.valid(test_json)).to be_truthy
+    expect(type).to eq(Hash)
   end
 
-  it 'returns false if json string is not valid' do
+  it 'tests if json string is invalid and throws an exception' do
     test_json = '{"id": 1 "firstname": "Berry"}'
-    expect(JsonConversions::Validate.valid?(test_json)).to eq(false)
+    expect { JsonConversions::Handler.valid(test_json) }.to raise_error(/Error: wrong filetype or invalid json/)
   end
+
+  it 'takes a valid json file and returns the result if valid' do
+    test_json = './spec/files/valid.json'
+    type = JsonConversions::Handler.valid(test_json).class
+    expect(JsonConversions::Handler.valid(test_json)).to be_truthy
+    expect(type).to eq(Hash)
+  end
+
+  it 'tests invalid json file and raises an exception' do
+    test_json = './spec/files/invalid.json'
+    expect { JsonConversions::Handler.valid(test_json) }.to raise_error(/Error: wrong filetype or invalid json/)
+  end
+
+  it 'turns key value pairs into simple html' do
+    key = 'somekey'
+    value = 'Some Value'
+    return_html = "<div class='#{key}'> <strong> #{key.capitalize} </strong> #{value} </div>"
+    expect(JsonConversions::Html.markup(key, value)).to eq(return_html)
+  end
+
 end
